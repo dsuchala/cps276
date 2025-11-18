1d6b1ac9c4653d8b71566ff663a2ba5d29f1dc42
Dylan Suchala <dsuchala@wccnet.edu>
2025-11-17 20:34:24 -0500

    Add weather API form to index.php

---
+++ Assignment10/index.php
@@ -0,0 +1,64 @@
+<?php
+$output = "";
+$acknowledgement = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require 'php/rest_client.php';
+    $result = getWeather();
+    $acknowledgement = $result[0];
+    $output = $result[1];
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+          rel="stylesheet" 
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+          crossorigin="anonymous">
+
+    <title>Weather API</title>
+
+    <style>
+        body {
+            margin-top: 40px;
+        }
+        .results-box {
+            margin-top: 30px;
+        }
+    </style>
+  </head>
+
+<body class="container">
+
+    <h1 class="mb-4">Enter Zip Code to Get City Weather</h1>
+    <?php echo $acknowledgement; ?>
+
+    <form method="post" action="index.php" class="form-inline">
+
+        <div class="form-group mb-3">
+            <label for="zip_code" class="form-label">Zip Code:</label>
+            <input 
+                type="text" 
+                class="form-control" 
+                id="zip_code" 
+                name="zip_code"
+                style="width: 25%;"
+                required>
+        </div>
+
+        <button type="submit" class="btn btn-primary mb-3">Submit</button>
+    </form>
+
+    <div class="results-box">
+        <?php echo $output; ?>
+    </div>
+
+</body>
+</html>
